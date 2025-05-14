12a9d270c3e7f1dbd3ed5a3dd8f429f32f30bdc7
JimmyCodeMunity <jameswafula2002@gmail.com>
2025-05-14 12:59:13 +0300

    with vercel

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["@emotion/react", "@emotion/styled", "@emotion/cache"],
-  },
-  // resolve: {
-  //   alias: {
-  //     '@mui/styled-engine': '@mui/styled-engine-sc',
-  //   },
+  // optimizeDeps: {
+  //   include: ["@emotion/react", "@emotion/styled", "@emotion/cache"],
   // },
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    },
+  },
 });
