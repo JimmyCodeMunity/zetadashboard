de43061ab0bfae408fc651229232a00207a97268
JimmyCodeMunity <jameswafula2002@gmail.com>
2025-05-14 12:45:53 +0300

    with vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@mui/styled-engine": "@mui/styled-engine-sc", // If you're using styled-components
+      // OR if you're using emotion, make sure not aliased incorrectly
+    },
+  },
+});
